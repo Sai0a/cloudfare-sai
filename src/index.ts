4027b5bc172b8fa07c8a39eaef68016e21bbe8e6
Hardik888 <hardik89990@gmail.com>
2025-12-31 15:06:42 +0530

    Change API

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Sai Upadhyay ur api is ready')
+app.get('/api/sai', (c) => {
+  return c.json({
+    message:'Hello Sai Upadhyay ur api is ready',
+    time: Date.now(),
+    data: {
+      id:1,
+      name:"Sai"
+    }
+  })
 })
 
 export default app
